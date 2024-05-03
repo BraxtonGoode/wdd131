0a3c13b4cb11b2f39ec6ccf7831cd2685a0d7f41
BraxtonGoode <tjbgoode@gmail.com>
2024-05-02 22:26:13 -0700

    temple album creation

---
+++ scripts/temples.js
@@ -0,0 +1,11 @@
+const currentyear = document.querySelector("#currentyear");
+const lastModified = document.querySelector("#lastModified")
+
+const today = new Date();
+const lastMod = document.lastModified;
+
+
+currentyear.innerHTML = `${today.getFullYear()}`;
+
+lastModified.innerHTML = `Last Modification: ${lastMod}`;
+// ---------------------------------------------------------
