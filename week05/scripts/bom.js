feef0bc15bdb9a424d5bb7ee73afe7b87c980360
BraxtonGoode <tjbgoode@gmail.com>
2024-05-23 12:43:55 -0700

    learning activity finished

---
+++ week05/scripts/bom.js
@@ -0,0 +1,82 @@
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document.querySelector("#list");
+
+
+
+const li = document.createElement("li");
+const deleteButton = document.createElement("button")
+
+li.textContent = input.value;
+
+deleteButton.textContent = "x";
+
+const chaptersArray = getChapterList() || []
+
+function setChapterList() {
+    localStorage.setItem('myFavBOMList', JSON.stringify(chaptersArray));
+  }
+function getChapterList(){
+    return JSON.parse(localStorage.getItem("myFavBOMList"))
+}
+
+function deleteChapter(chapter) {
+    chapter = chapter.slice(0,chapter.length-1)
+    chaptersArray = chaptersArray.filter((item)=> item !== chapter)
+    setChapterList()
+}
+
+
+const displayList = function(item) {
+        
+
+        const li = document.createElement("li");
+        const deleteButton = document.createElement("button")
+        li.textContent = item;
+        deleteButton.textContent = "x";
+        deleteButton.classList.add("delete")
+        li.append(deleteButton)
+        list.append(li)
+  
+        deleteButton.addEventListener('click', function () {
+            
+            list.removeChild(li);
+            deleteChapter(li.textContent)
+            input.focus();
+            
+        });
+        
+}
+
+
+
+
+
+
+
+chaptersArray.forEach(chapter => {
+    displayList(chapter)
+});
+
+
+
+
+
+
+
+
+// add chapter
+
+button.addEventListener("click", function() {
+    if (input.value.trim() !== ""){
+        
+        displayList(input.value)
+        chaptersArray.push(input.value)
+        setChapterList()
+        input.value = ""
+        input.focus()
+
+    }
+
+});
+
