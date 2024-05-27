b3a122b051fbe94e188910ff60b17d1b8d159df1
BraxtonGoode <tjbgoode@gmail.com>
2024-05-27 15:07:45 -0700

    email functionality

---
+++ includes/send_email.php
@@ -0,0 +1,25 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    $name = $_GET['name'];
+    $email = $_GET['email'];
+    $message = $_GET['textarea'];
+
+    // Email address where you want to receive the message
+    $to = "tjbgoode@gmail.com";
+
+    // Subject of the email
+    $subject = "New Message from Contact Form";
+
+    // Email content
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n\n";
+    $email_content .= "Message:\n$message\n";
+
+    // Send email
+    mail($to, $subject, $email_content);
+
+    // Redirect user to a thank you page
+    header("Location: Contacted.html");
+    exit();
+}
+?>
